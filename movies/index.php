4d6124c9efaf32d21f1b7197ada5b70e07be8737
Gunnar Bittersmann <gunnar@bittersmann.de>
2023-01-28 20:05:23 +0100

    feat: add script

--- movies/index.php
+++ movies/index.php
@@ -7,6 +7,7 @@
   const FAVICON = STARFLEET_LOGO;
   const APPLE_TOUCH_ICON = '../apple-touch-icon.png';
   const STYLESHEET = '../style.css?date=2022-10-12T13:13Z';
+  const SCRIPT = '../script.js';
 
   $json = file_get_contents('movies.jsonld');
   $data = json_decode($json, TRUE);
@@ -255,5 +256,8 @@
         <?php endforeach; ?>
       </table>
     </main>
+    <script>
+      <?php readfile(SCRIPT); ?>
+    </script>
   </body>
 </html>
