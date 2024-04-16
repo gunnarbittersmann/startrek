bd0366f346299d7ccfd043bff4c06cbb598a7b8e
Gunnar Bittersmann <gunnar@bittersmann.de>
2024-04-16 13:33:30 +0200

    feat: abbreviate reviewer

--- movies/index.php
+++ movies/index.php
@@ -170,10 +170,12 @@
 											<details lang="en" property="video" typeof="VideoObject">
 												<summary aria-describedby="<?= htmlSpecialChars($movie['@identifier']) ?>">
 													<?php if ($movie['review']['name']): ?>
-														<span property="name"><?= htmlSpecialChars($movie['review']['name']) ?></span>
+														<span class="visually-hidden" property="name"><?= htmlSpecialChars($movie['review']['name']) ?></span>
+														<abbr aria-hidden="true"><?= htmlSpecialChars($movie['review']['name'][0]) ?></abbr>
 													<?php elseif ($movie['review']['creator'] && $movie['review']['creator']['name']): ?>
 														<span property="creator" typeof="<?= htmlSpecialChars($movie['review']['creator']['@type']) ?>">
-															<span property="name"><?= htmlSpecialChars($movie['review']['creator']['name']) ?></span>
+															<span class="visually-hidden" property="name"><?= htmlSpecialChars($movie['review']['creator']['name']) ?></span>
+															<abbr aria-hidden="true"><?= htmlSpecialChars($movie['review']['creator']['name'][0]) ?></abbr>
 														</span>
 													<?php endif; ?>
 												</summary>
@@ -196,10 +198,12 @@
 														<details lang="en" property="video" typeof="VideoObject">
 															<summary aria-describedby="<?= htmlSpecialChars($movie['@identifier']) ?>">
 																<?php if ($review['name']): ?>
-																	<span property="name"><?= htmlSpecialChars($review['name']) ?></span>
+																	<span class="visually-hidden" property="name"><?= htmlSpecialChars($review['name']) ?></span>
+																	<abbr aria-hidden="true"><?= htmlSpecialChars($review['name'][0]) ?></abbr>
 																<?php elseif ($review['creator'] && $review['creator']['name']): ?>
 																	<span property="creator" typeof="<?= htmlSpecialChars($review['creator']['@type']) ?>">
-																		<span property="name"><?= htmlSpecialChars($review['creator']['name']) ?></span>
+																		<span class="visually-hidden" property="name"><?= htmlSpecialChars($review['creator']['name']) ?></span>
+																		<abbr aria-hidden="true"><?= htmlSpecialChars($review['creator']['name'][0]) ?></abbr>
 																	</span>
 																<?php endif; ?>
 															</summary>
