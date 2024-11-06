8e022d48f6517c80b782224be36a77b6e0811b8b
Gunnar Bittersmann <gunnar@bittersmann.de>
2024-11-06 13:43:09 +0100

    feat: render teleplay and story authors

--- series/index.php
+++ series/index.php
@@ -215,28 +215,87 @@ EOT;
 										<?php endif; ?>
 										<?php if (IS_AUTHOR_VISIBLE): ?>
 											<?php if ($episode['author']): ?>
-												<?php if ($episode['author']['name']): ?>
-													<td
-														property="author"
-														typeof="<?= htmlSpecialChars($episode['author']['@type']) ?>"
-														resource="https://bittersmann.de/startrek/persons/<?= htmlSpecialChars($episode['author']['@id']) ?>"
-													>
-														<span property="name"><?= htmlSpecialChars($episode['author']['name']) ?></span>
+												<?php if ($episode['isBasedOn'] && $episode['isBasedOn']['author']): ?>
+													<td>
+														<dl>
+															<div>
+																<dt>teleplay by:</dt>
+																<?php if ($episode['author']['name']): ?>
+																	<dd
+																		property="author"
+																		typeof="<?= htmlSpecialChars($episode['author']['@type']) ?>"
+																		resource="https://bittersmann.de/startrek/persons/<?= htmlSpecialChars($episode['author']['@id']) ?>"
+																	>
+																		<span property="name"><?= htmlSpecialChars($episode['author']['name']) ?></span>
+																	</dd>
+																<?php else: ?>
+																	<dd>
+																		<ul>
+																			<?php foreach ($episode['author'] as $author): ?>
+																				<li
+																					property="author"
+																					typeof="<?= htmlSpecialChars($author['@type']) ?>"
+																					resource="https://bittersmann.de/startrek/persons/<?= htmlSpecialChars($author['@id']) ?>"
+																				>
+																					<span property="name"><?= htmlSpecialChars($author['name']) ?></span>
+																				</li>
+																			<?php endforeach; ?>
+																		</ul>
+																	</dd>
+																<?php endif; ?>
+															</div>
+															<div>
+																<dt>story by:</dt>
+																<?php if ($episode['isBasedOn']['author']['name']): ?>
+																	<dd
+																		property="author"
+																		typeof="<?= htmlSpecialChars($episode['isBasedOn']['author']['@type']) ?>"
+																		resource="https://bittersmann.de/startrek/persons/<?= htmlSpecialChars($episode['isBasedOn']['author']['@id']) ?>"
+																	>
+																		<span property="name"><?= htmlSpecialChars($episode['isBasedOn']['author']['name']) ?></span>
+																	</dd>
+																<?php else: ?>
+																	<dd>
+																		<ul>
+																			<?php foreach ($episode['isBasedOn']['author'] as $author): ?>
+																				<li
+																					property="author"
+																					typeof="<?= htmlSpecialChars($author['@type']) ?>"
+																					resource="https://bittersmann.de/startrek/persons/<?= htmlSpecialChars($author['@id']) ?>"
+																				>
+																					<span property="name"><?= htmlSpecialChars($author['name']) ?></span>
+																				</li>
+																			<?php endforeach; ?>
+																		</ul>
+																	</dd>
+																<?php endif; ?>
+															</div>
+														</dl>
 													</td>
 												<?php else: ?>
-													<td>
-														<ul>
-															<?php foreach ($episode['author'] as $author): ?>
-																<li
-																	property="author"
-																	typeof="<?= htmlSpecialChars($author['@type']) ?>"
-																	resource="https://bittersmann.de/startrek/persons/<?= htmlSpecialChars($author['@id']) ?>"
-																>
-																	<span property="name"><?= htmlSpecialChars($author['name']) ?></span>
-																</li>
-															<?php endforeach; ?>
-														</ul>
-													</td>
+													<?php if ($episode['author']['name']): ?>
+														<td
+															property="author"
+															typeof="<?= htmlSpecialChars($episode['author']['@type']) ?>"
+															resource="https://bittersmann.de/startrek/persons/<?= htmlSpecialChars($episode['author']['@id']) ?>"
+														>
+															<span property="name"><?= htmlSpecialChars($episode['author']['name']) ?></span>
+														</td>
+													<?php else: ?>
+														<td>
+															<ul>
+																<?php foreach ($episode['author'] as $author): ?>
+																	<li
+																		property="author"
+																		typeof="<?= htmlSpecialChars($author['@type']) ?>"
+																		resource="https://bittersmann.de/startrek/persons/<?= htmlSpecialChars($author['@id']) ?>"
+																	>
+																		<span property="name"><?= htmlSpecialChars($author['name']) ?></span>
+																	</li>
+																<?php endforeach; ?>
+															</ul>
+														</td>
+													<?php endif; ?>
 												<?php endif; ?>
 											<?php endif; ?>
 										<?php endif; ?>
