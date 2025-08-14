d2137d32f36011c05044665f6c0a3c81b41511a6
Gunnar Bittersmann <gunnar@bittersmann.de>
2025-08-14 20:03:34 +0200

    feat: have summaries in line in Safari

--- series/index.php
+++ series/index.php
@@ -9,7 +9,7 @@
 	const STARFLEET_LOGO = '../starfleet.svg';
 	const FAVICON = STARFLEET_LOGO;
 	const APPLE_TOUCH_ICON = '../apple-touch-icon.png';
-	const STYLESHEET = '../style.css?date=2025-08-04T10:36Z';
+	const STYLESHEET = '../style.css?date=2025-08-14T18:04Z';
 	const SCRIPT = '../script.js';
 
 	$files = scandir('.');
