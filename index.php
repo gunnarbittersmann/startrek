4d0243e91bc45aa1e0b2c74254df7369c687ad1d
Gunnar Bittersmann <gunnar@bittersmann.de>
2025-10-27 03:05:04 +0100

    feat: add Khan to main menu

--- index.php
+++ index.php
@@ -88,6 +88,14 @@
 					</ol>
 				</li>
 				<li><a href="movies">Movies</a></li>
+				<li>
+					<a>Audio drama</a>
+					<ul>
+						<li>
+							<a href="audiodrama/khan">Khan</a>
+						</li>
+					</ul>
+				</li>
 				<li><a href="books">Books</a></li>
 				<li><a href="timelines">Timelines</a></li>
 				<li>
