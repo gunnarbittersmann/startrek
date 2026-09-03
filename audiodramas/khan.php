36499099a2eb065b355e37c5d16a10737ceeb32f
Gunnar Bittersmann <gunnar@bittersmann.de>
2026-09-03 11:40:12 +0200

    refactoring: simplify meta viewport

--- audiodramas/khan.php
+++ audiodramas/khan.php
@@ -11,7 +11,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<meta name="viewport" content="width=device-width"/>
 		<title>Star Trek: Khan</title>
 		<link rel="icon" href="../starfleet.svg"/>
 		<link rel="mask-icon" href="../starfleet.svg"/>
