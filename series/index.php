3aabea32bf973e4abc236e89db99c39e9e65b1ba
Gunnar Bittersmann <gunnar@bittersmann.de>
2026-09-06 00:28:19 +0200

    feat: display ratings on series reviews

--- series/index.php
+++ series/index.php
@@ -890,19 +890,37 @@ EOT;
 													aria-description="season <?= htmlSpecialChars($data['name']) ?>"
 												>
 												</iframe>
-												<?php if ($data['review']['name'] || $data['review']['datePublished']): ?>
+												<?php if ($data['review']['name'] || $data['review']['datePublished'] || ($data['review']['reviewRating'] && $data['review']['reviewRating']['ratingValue'])): ?>
 													<p>
-														<span property="name"><?= htmlSpecialChars($data['review']['name']) ?></span>
-														<time
-															property="datePublished"
-															<?php if ($data['review']['datePublished'] > date_format(date_create('- 2 days'), 'Y-m-d')): ?>
-																class="new"
-															<?php endif; ?>
-														>
-															<?= htmlSpecialChars($data['review']['datePublished']) ?>
-														</time>
+														<?php if ($data['review']['name']): ?>
+															<span property="name"><?= htmlSpecialChars($data['review']['name']) ?></span>
+														<?php endif; ?>
+														<?php if ($data['review']['datePublished']): ?>
+															<time
+																property="datePublished"
+																<?php if ($data['review']['datePublished'] > date_format(date_create('- 2 days'), 'Y-m-d')): ?>
+																	class="new"
+																<?php endif; ?>
+															>
+																<?= htmlSpecialChars($data['review']['datePublished']) ?>
+															</time>
+														<?php endif; ?>
+														<?php if ($data['review']['reviewRating'] && $data['review']['reviewRating']['ratingValue']): ?>
+															<span property="reviewRating" typeof="<?= htmlSpecialChars($data['review']['reviewRating']['@type']) ?>">
+																rating:
+																<?php if ($data['review']['reviewRating']['bestRating']): ?>
+																	<span property="ratingValue"><?=
+																		htmlSpecialChars($data['review']['reviewRating']['ratingValue'])
+																	?></span>/<span property="bestRating"><?=
+																		htmlSpecialChars($data['review']['reviewRating']['bestRating'])
+																	?></span>
+																<?php else: ?>
+																	<span property="ratingValue"><?= htmlSpecialChars($data['review']['reviewRating']['ratingValue']) ?></span>
+																<?php endif; ?>
+															</span>
+														<?php endif; // ($data['review']['reviewRating'] && $data['review']['reviewRating']['ratingValue']) ?>
 													</p>
-												<?php endif; ?>
+												<?php endif; // ($data['review']['name'] || $data['review']['datePublished'] || ($data['review']['reviewRating'] && $review['reviewRating']['ratingValue'])) ?>
 											</div>
 										</details>
 									</td>
@@ -955,19 +973,37 @@ EOT;
 																	aria-description="season <?= htmlSpecialChars($data['name']) ?>"
 																>
 																</iframe>
-																<?php if ($review['name'] || $review['datePublished']): ?>
+																<?php if ($review['name'] || $review['datePublished'] || ($review['reviewRating'] && $review['reviewRating']['ratingValue'])): ?>
 																	<p>
-																		<span property="name"><?= htmlSpecialChars($review['name']) ?></span>
-																		<time
-																			property="datePublished"
-																			<?php if ($review['datePublished'] > date_format(date_create('- 2 days'), 'Y-m-d')): ?>
-																				class="new"
-																			<?php endif; ?>
-																		>
-																			<?= htmlSpecialChars($review['datePublished']) ?>
-																		</time>
+																		<?php if ($review['name']): ?>
+																			<span property="name"><?= htmlSpecialChars($review['name']) ?></span>
+																		<?php endif; ?>
+																		<?php if ($review['datePublished']): ?>
+																			<time
+																				property="datePublished"
+																				<?php if ($review['datePublished'] > date_format(date_create('- 2 days'), 'Y-m-d')): ?>
+																					class="new"
+																				<?php endif; ?>
+																			>
+																				<?= htmlSpecialChars($review['datePublished']) ?>
+																			</time>
+																		<?php endif; ?>
+																		<?php if ($review['reviewRating'] && $review['reviewRating']['ratingValue']): ?>
+																			<span property="reviewRating" typeof="<?= htmlSpecialChars($review['reviewRating']['@type']) ?>">
+																				rating:
+																				<?php if ($review['reviewRating']['bestRating']): ?>
+																					<span property="ratingValue"><?=
+																						htmlSpecialChars($review['reviewRating']['ratingValue'])
+																					?></span>/<span property="bestRating"><?=
+																						htmlSpecialChars($review['reviewRating']['bestRating'])
+																					?></span>
+																				<?php else: ?>
+																					<span property="ratingValue"><?= htmlSpecialChars($review['reviewRating']['ratingValue']) ?></span>
+																				<?php endif; ?>
+																			</span>
+																		<?php endif; // ($review['reviewRating'] && $review['reviewRating']['ratingValue']) ?>
 																	</p>
-																<?php endif; ?>
+																<?php endif; // ($review['name'] || $review['datePublished'] || ($review['reviewRating'] && $review['reviewRating']['ratingValue'])) ?>
 															</div>
 														</details>
 													</li>
